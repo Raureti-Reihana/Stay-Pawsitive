3694634f44b49f0b5028c20b58401f1cd6db36e8
Trisha <trishaemperado@gmail.com>
2024-06-19 09:34:27 +1000

    set-up fetchDog API

---
+++ client/APIs/fetchDog.ts
@@ -0,0 +1,9 @@
+import request from 'superagent'
+
+export async function fetchDog() {
+  const res = await request.get(
+    `https://dog.ceo/api/breeds/image/random/3`,
+  )
+
+  return res.body 
+}
