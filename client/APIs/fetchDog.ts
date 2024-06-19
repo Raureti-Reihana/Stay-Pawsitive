422de2db2aabde40d225722dd148ffc0a9d3796c
Om Mistry <ommistry03@gmail.com>
2024-06-19 13:44:33 +1200

    Running test cause issues3

--- client/APIs/fetchDog.ts
+++ client/APIs/fetchDog.ts
@@ -1,9 +1,7 @@
 import request from 'superagent'
 
 export async function fetchDog() {
-  const res = await request.get(
-    `https://dog.ceo/api/breeds/image/random/3`,
-  )
+  const res = await request.get(`https://dog.ceo/api/breeds/image/random/3`)
 
-  return res.body 
+  return res.body
 }
