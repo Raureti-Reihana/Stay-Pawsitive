0bb345c0003d38b71a1b2acb07a10ce1e13bc724
Christy Liu <liusiyang915@gmail.com>
2024-06-19 23:19:43 +1200

    added a button on the page

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,28 @@
 function App() {
+
+  const refreshPage = () => {
+    window.location.reload();
+  }
+
   return (
     <div>
       <h1>App</h1>
       <p>React development has begun!</p>
+      <button 
+        onClick={refreshPage} 
+        style={{ 
+          padding: '10px 20px', 
+          fontSize: '16px', 
+          backgroundColor: '#ff4500', 
+          color: 'white', 
+          border: 'none', 
+          borderRadius: '5px', 
+          cursor: 'pointer',
+          marginTop: '20px'
+        }}
+      >
+        Get a New Dog Picture
+      </button>
     </div>
   )
 }
