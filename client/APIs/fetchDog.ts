a1d4015ec9fa57c032b449466bfa6a4313ee9885
Raureti.Reihana <raureti1@gmail.com>
2024-06-20 12:19:15 +1200

    edit api fetcher

--- client/APIs/fetchDog.ts
+++ client/APIs/fetchDog.ts
@@ -1,10 +1,13 @@
 import request from 'superagent'
+import { dogPictures } from '../models/fetchDog'
 
+export async function fetchDog(): Promise<dogPictures> {
+  try {
+    const res = await request.get(`https://dog.ceo/api/breeds/image/random`)
 
-export async function fetchDog(): Promise<{ message: string}> {
-  const res = await request.get(
-    `https://dog.ceo/api/breeds/image/random`,
-  )
-
-  return res.body
+    return res.body
+  } catch (error) {
+    console.error('Error finding dog', error)
+    throw error
+  }
 }
