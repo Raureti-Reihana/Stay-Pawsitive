fbd6797ad79deafc408f51488225df468967a5c5
Christy Liu <liusiyang915@gmail.com>
2024-06-22 16:02:10 +1200

    added NavBar etc

---
+++ client/routing.tsx
@@ -0,0 +1,33 @@
+// routing.tsx
+import { Route, createBrowserRouter, createRoutesFromElements, Navigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import App from './components/App';
+import Comments from './components/comments';
+import Dogs from './components/Dogs';
+
+
+
+const DogsWrapper: React.FC = () => {
+  const [currentDog, setCurrentDog] = useState<string>('');
+
+  return <Dogs currentDog={currentDog} setCurrentDog={setCurrentDog} />;
+};
+
+const CommentsWrapper: React.FC = () => {
+  const [currentDog] = useState<string>('');
+  const [commentsMap, setCommentsMap] = useState<Record<string, string[]>>({});
+
+  return <Comments currentDog={currentDog} commentsMap={commentsMap} setCommentsMap={setCommentsMap} />;
+};
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="dogs" element={<DogsWrapper />} />
+      <Route path="comments" element={<CommentsWrapper />} />
+      <Route path="index" element={<Navigate to="/" replace />} />
+    </Route>
+  )
+);
+
+export default router
